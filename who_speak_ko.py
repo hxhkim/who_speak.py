4016e590af4f987556db79562048a4e616108134
Hahyun Kim <68641751+hxhkim@users.noreply.github.com>
2023-10-31 12:10:28 +0900

    Update who_speak_ko.py

--- who_speak_ko.py
+++ who_speak_ko.py
@@ -3,7 +3,7 @@
 def who_speaking():
     import random
 
-    members = ["강민정", "김하현", "박수빈", "안인균", "이주선", "심재호"]
+    members = input("팀원 이름을 띄어쓰기 단위로 입력하세요 (예, 박지성 유재석): ").split()
 
     random.shuffle(members)
     speaking_member = members[0]
@@ -11,4 +11,4 @@ def who_speaking():
     return speaking_member
 
 result = who_speaking()
-print(result)
+print(f"발표자는 {result}입니다.")
